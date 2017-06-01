307cd16ba9af276e8e7b462c9fce52e1040ad0cb
MuddyBootsCode <firsttogo98@gmail.com>
2017-06-01 15:17:12 +0000

    created geneva_record

---
+++ app/models/geneva_record.rb
@@ -0,0 +1,2 @@
+class GenevaRecord < ApplicationRecord
+end
---
+++ test/models/geneva_record_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GenevaRecordTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
