648b467372f52e884e48912d0e008122e0a41210
MuddyBootsCode <firsttogo98@gmail.com>
2017-06-07 00:52:05 +0000

    completed simple form fields

--- app/controllers/geneva_records_controller.rb
+++ app/controllers/geneva_records_controller.rb
@@ -8,4 +8,24 @@ class GenevaRecordsController < ApplicationController
     @genevarecord = GenevaRecord.new
   end
 
+  def create
+    GenevaRecord.create(record_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def record_params
+
+    params.require(:geneva_record).permit(
+      :GV_01_open_gauge,
+      :GV_01_open_stock,
+      :GV_01_close_gauge,
+      :GV_01_close_stock,
+      :GV_01_stock_change
+      )
+
+
+  end
+   
 end
