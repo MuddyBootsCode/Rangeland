4fb9e4505b5e7c7b3037503ecf52495dccbf2346
MuddyBootsCode <firsttogo98@gmail.com>
2017-06-08 23:31:39 +0000

    added all GV vessels to index and forms

--- app/controllers/geneva_records_controller.rb
+++ app/controllers/geneva_records_controller.rb
@@ -24,7 +24,15 @@ class GenevaRecordsController < ApplicationController
       :GV_01_open_gauge,
       :GV_01_open_stock,
       :GV_01_close_gauge,
-      :GV_01_close_stock
+      :GV_01_close_stock,
+      :GV_02_open_gauge,
+      :GV_02_open_stock,
+      :GV_02_close_gauge,
+      :GV_02_close_stock,
+      :GV_03_open_gauge,
+      :GV_03_open_stock,
+      :GV_03_close_gauge,
+      :GV_03_close_stock
       )
   end
 
--- db/migrate/20170601150515_create_geneva_records.rb
+++ db/migrate/20170601150515_create_geneva_records.rb
@@ -8,6 +8,16 @@ class CreateGenevaRecords < ActiveRecord::Migration[5.0]
       t.float :GV_01_open_stock
       t.float :GV_01_close_stock
       t.float :GV_01_stock_change
+      t.text :GV_02_open_gauge
+      t.text :GV_02_close_gauge
+      t.float :GV_02_open_stock
+      t.float :GV_02_close_stock
+      t.float :GV_02_stock_change
+      t.text :GV_03_open_gauge
+      t.text :GV_03_close_gauge
+      t.float :GV_03_open_stock
+      t.float :GV_03_close_stock
+      t.float :GV_03_stock_change
       t.timestamps
     end
   end
