64dc539f932c3d66dc6d95e4dcd02431725fd239
MuddyBootsCode <firsttogo98@gmail.com>
2017-06-13 15:18:34 +0000

    made some form changes

--- app/models/geneva_record.rb
+++ app/models/geneva_record.rb
@@ -1,19 +1,5 @@
 class GenevaRecord < ApplicationRecord
 
-
-  def schange
-    self.GV_01_close_stock - self.GV_01_open_stock
-  end
-
-  def schange2
-    self.GV_02_close_stock - self.GV_02_open_stock
-    end
-
-  def schange3
-    self.GV_03_close_stock - self.GV_03_open_stock
-  end
-
-
-
+  
 end
 
